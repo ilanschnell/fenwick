085e2391d98de478af9c73bd1d51d557a4e4afb6
Ilan Schnell <ilanschnell@gmail.com>
2021-02-04 03:02:26 -0600

    add unittests

--- example.py
+++ example.py
@@ -1,32 +1,13 @@
-from random import randint
-from pprint import pprint
-
 from fenwick import Fenwick
 
-N = 15
-a = [randint(-N, N) for _ in range(N)]
-sm = 0
-s = []
-for i in range(N):
-    sm += a[i]
-    s.append(sm)
-pprint(s)
-
-f = Fenwick(N)
-pprint(f.values())
-for i, v in enumerate(a):
-    f.add(i, v)
-pprint([f.prefix_sum(i + 1) for i in range(N)])
-pprint(a)
-pprint([f[i] for i in range(N)])
-pprint(f.values())
+a = [2, 11, 4, -5]
 
 f = Fenwick(a)
-pprint([f[i] for i in range(N)])
-pprint(a)
-
-for _ in range(1000):
-    i = randint(0, N - 1)
-    j = randint(i, N - 1)
-    #print(i, j)
-    assert f.range_sum(i, j) == sum(a[i:j])
+print(f.values())
+f.add(2, 3)
+print(f.values())
+f[1] = 15
+print(f.values())
+print(f[3])
+print(f.prefix_sum(3))
+print(f.range_sum(1, 3))
--- test_fenwick.py
+++ test_fenwick.py
@@ -1,32 +1,39 @@
 from random import randint
-from pprint import pprint
+import unittest
 
 from fenwick import Fenwick
 
-N = 15
-a = [randint(-N, N) for _ in range(N)]
-sm = 0
-s = []
-for i in range(N):
-    sm += a[i]
-    s.append(sm)
-pprint(s)
 
-f = Fenwick(N)
-pprint(f.values())
-for i, v in enumerate(a):
-    f.add(i, v)
-pprint([f.prefix_sum(i + 1) for i in range(N)])
-pprint(a)
-pprint([f[i] for i in range(N)])
-pprint(f.values())
+class TestsFenwick(unittest.TestCase):
 
-f = Fenwick(a)
-pprint([f[i] for i in range(N)])
-pprint(a)
+    def test_init_0(self):
+        f = Fenwick(0)
+        self.assertEqual(len(f), 0)
+        self.assertEqual(f.values(), [])
 
-for _ in range(1000):
-    i = randint(0, N - 1)
-    j = randint(i, N - 1)
-    #print(i, j)
-    assert f.range_sum(i, j) == sum(a[i:j])
+    def test_1(self):
+        N = 15
+        a = [randint(-N, N) for _ in range(N)]
+        sm = 0
+        s = []
+        for i in range(N):
+            sm += a[i]
+            s.append(sm)
+        f = Fenwick(N)
+        for i, v in enumerate(a):
+            f.add(i, v)
+        self.assertEqual([f.prefix_sum(i + 1) for i in range(N)], s)
+        self.assertEqual(f.values(), a)
+
+    def test_2(self):
+        for n in range(20):
+            a = [randint(-100, 100) for _ in range(n)]
+            f = Fenwick(a)
+            self.assertEqual([f[i] for i in range(n)], a)
+            for _ in range(10 * n):
+                i = randint(0, n - 1)
+                j = randint(i, n - 1)
+                self.assertEqual(f.range_sum(i, j), sum(a[i:j]))
+
+if __name__ == '__main__':
+    unittest.main()
